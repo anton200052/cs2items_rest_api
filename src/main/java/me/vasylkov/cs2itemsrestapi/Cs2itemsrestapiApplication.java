42330fb18c190fc17590dc2708d0daa903f8dec0
Anton Vasylkov <neatrabe345@gmail.com>
2024-04-18 16:10:43 +0200

    Init commit

---
+++ src/main/java/me/vasylkov/cs2itemsrestapi/Cs2itemsrestapiApplication.java
@@ -0,0 +1,13 @@
+package me.vasylkov.cs2itemsrestapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cs2itemsrestapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Cs2itemsrestapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/me/vasylkov/cs2itemsrestapi/Cs2itemsrestapiApplicationTests.java
@@ -0,0 +1,13 @@
+package me.vasylkov.cs2itemsrestapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cs2itemsrestapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
